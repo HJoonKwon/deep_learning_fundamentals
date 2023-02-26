076d7bc51d0f3f3f296c6f283307014516852fba
HJoonKwon <dsazz801@gmail.com>
2023-02-26 17:35:08 +0900

    feat: build patch embedding layer for swin

--- sequence_models/swin_transformer.py
+++ sequence_models/swin_transformer.py
@@ -2,6 +2,7 @@ import collections
 from itertools import repeat
 import torch
 import torch.nn as nn
+import einops
 
 
 '''
@@ -12,6 +13,7 @@ import torch.nn as nn
 4. https://github.com/huggingface/pytorch-image-models/blob/450b4affbd73393ee02b075d6298ef29b2eca74/timm/layers/helpers.py#L10
 '''
 
+
 def _ntuple(n):
     def parse(x):
         if isinstance(x, collections.abc.Iterable) and not isinstance(x, str):
@@ -19,15 +21,17 @@ def _ntuple(n):
         return tuple(repeat(x, n))
     return parse
 
-to_2tuple =_ntuple(2)
 
+to_2tuple = _ntuple(2)
 
 
 def window_partition():
     pass
 
+
 def window_reverse():
     pass
+
 
 class PatchMerging(nn.Module):
     def __init__(self) -> None:
@@ -36,6 +40,7 @@ class PatchMerging(nn.Module):
     def forward(self, x):
         pass
 
+
 class Mlp(nn.Module):
     def __init__(self) -> None:
         super().__init__()
@@ -43,12 +48,34 @@ class Mlp(nn.Module):
     def forward(self, x):
         pass
 
+
 class PatchEmbedding(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self,
+                 img_size=224,
+                 patch_dim=4,
+                 in_channels=3,
+                 dim_embed=512) -> None:
         super().__init__()
+        self.img_size = to_2tuple(img_size)
+        self.patch_dim = to_2tuple(patch_dim)
+        patches_resolution = [img_size[0] //
+                              patch_dim[0], img_size[1]//patch_dim[1]]
+        num_patches = patches_resolution[0] * patches_resolution[1]
+        dim_patch = patch_dim[0] * patch_dim[1] * in_channels
+        self.patches_resolution = patches_resolution
+        self.num_patches = num_patches
+        self.dim_patch = dim_patch
+        self.dim_embed = dim_embed
+
+        self.embedding = nn.Conv2d(
+            in_channels, dim_embed, kernel_size=self.patch_dim, stride=self.patch_dim)
 
     def forward(self, x):
-        pass
+        B, C, H, W = x.shape
+        embedded = self.embedding(x) # B x dim_embed x patches_resolution[0] x patches_resolution[1]
+        flattened = einops.rearrange(embedded, 'b d ph pw -> b (ph pw d)')
+        return flattened
+
 
 
 class WindowMultiHeadSelfAttention(nn.Module):
@@ -73,6 +100,7 @@ class SwinTransformerBlock(nn.Module):
     def forward(self, x):
         pass
 
+
 class SwinTransformerStage(nn.Module):
     def __init__(self) -> None:
         super().__init__()
@@ -87,4 +115,3 @@ class SwinTransformer(nn.Module):
 
     def forward(self, x):
         pass
-
