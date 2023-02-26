d9362c0344b5c273ee554f1a8e54123f63aac072
HJoonKwon <dsazz801@gmail.com>
2023-02-26 17:58:49 +0900

    rev: unit test for normalization on patch embedding

--- tests/test_swin.py
+++ tests/test_swin.py
@@ -34,3 +34,19 @@ def test_patch_embedding():
                               patch_dim[0], img_size[1]//patch_dim[1]]
     num_patches = patches_resolution[0] * patches_resolution[1]
     assert output.shape == (batch_size, num_patches, dim_embed)
+
+    img_size = (224, 112)
+    patch_dim = (8, 4)
+    embedding_module = PatchEmbedding(
+        img_size=img_size,
+        patch_dim=patch_dim,
+        in_channels=in_channels,
+        dim_embed=dim_embed,
+        norm_layer=nn.LayerNorm
+    )
+    img = torch.randn(batch_size, in_channels, *img_size)
+    output = embedding_module(img)
+    patches_resolution = [img_size[0] //
+                              patch_dim[0], img_size[1]//patch_dim[1]]
+    num_patches = patches_resolution[0] * patches_resolution[1]
+    assert output.shape == (batch_size, num_patches, dim_embed)
