f8ef8de8f21fff4a04691513f3c9c85bd71d1f51
HJoonKwon <dsazz801@gmail.com>
2023-02-26 17:13:19 +0900

    feat: add helper function

--- sequence_models/swin_transformer.py
+++ sequence_models/swin_transformer.py
@@ -1,3 +1,5 @@
+import collections
+from itertools import repeat
 import torch
 import torch.nn as nn
 
@@ -9,6 +11,17 @@ import torch.nn as nn
 3. https://towardsdatascience.com/a-comprehensive-guide-to-swin-transformer-64965f89d14c
 4. https://github.com/huggingface/pytorch-image-models/blob/450b4affbd73393ee02b075d6298ef29b2eca74/timm/layers/helpers.py#L10
 '''
+
+def _ntuple(n):
+    def parse(x):
+        if isinstance(x, collections.abc.Iterable) and not isinstance(x, str):
+            return tuple(x)
+        return tuple(repeat(x, n))
+    return parse
+
+to_2tuple =_ntuple(2)
+
+
 
 def window_partition():
     pass
