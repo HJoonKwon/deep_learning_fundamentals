f818576a9cf70f10e7434ef10abfa675c2ba75a1
HJoonKwon <dsazz801@gmail.com>
2023-02-26 16:58:51 +0900

    feat: add skeleton for swin transformer

---
+++ sequence_models/swin_transformer.py
@@ -0,0 +1,76 @@
+import torch
+import torch.nn as nn
+
+
+'''
+<Rereferences>
+1. https://arxiv.org/abs/2103.14030
+2. https://github.com/microsoft/Swin-Transformer/blob/main/models/swin_transformer.py
+3. https://towardsdatascience.com/a-comprehensive-guide-to-swin-transformer-64965f89d14c
+'''
+
+def window_partition():
+    pass
+
+def window_reverse():
+    pass
+
+class PatchMerging(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+class Mlp(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+class PatchEmbedding(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+
+class WindowMultiHeadSelfAttention(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+class ShiftedWindowMultiHeadSelfAttention(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+
+class SwinTransformerBlock(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+class SwinTransformerStage(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+
+class SwinTransformer(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
