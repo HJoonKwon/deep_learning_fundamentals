e71d5fa1813e654237de3930d369da46203d8367
HJoonKwon <dsazz801@gmail.com>
2023-03-05 18:26:39 +0900

    fix: fix typo

--- sequence_models/swin_transformer.py
+++ sequence_models/swin_transformer.py
@@ -76,7 +76,7 @@ class Mlp(nn.Module):
         super().__init__()
         # self.layer = nn.Sequential()
         self.fc1 = nn.Linear(in_features, hidden_features)
-        self.act_layer = norm_layer()
+        self.act_layer = act_layer()
         self.fc2 = nn.Linear(hidden_features, out_features)
         self.drop = nn.Dropout(drop)
 
