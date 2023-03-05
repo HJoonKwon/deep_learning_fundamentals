bf021135e6aca9cbe5e78ddb3faed2494cfb843d
HJoonKwon <dsazz801@gmail.com>
2023-03-05 13:44:35 +0900

    feat: add original copyright

--- sequence_models/swin_transformer.py
+++ sequence_models/swin_transformer.py
@@ -1,8 +1,17 @@
-import torch
-import torch.nn as nn
-import einops
-
-from .utils import *
+# coding=utf-8
+# Copyright 2022 Microsoft Research and The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 '''
 <Rereferences>
@@ -10,7 +19,14 @@ from .utils import *
 2. https://github.com/microsoft/Swin-Transformer/blob/main/models/swin_transformer.py
 3. https://towardsdatascience.com/a-comprehensive-guide-to-swin-transformer-64965f89d14c
 4. https://github.com/huggingface/transformers/blob/v4.26.1/src/transformers/models/swin/modeling_swin.py
+5. https://github.com/huggingface/transformers/blob/main/src/transformers/models/swin/modeling_swin.py
 '''
+
+import torch
+import torch.nn as nn
+import einops
+
+from .utils import *
 
 def relative_position_index(window_size):
     pass
